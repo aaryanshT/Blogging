c6bd888a44a3422b0251e89351ce560cf99ec310
mrityunjay2003 <mrityunjay281203@gmail.com>
2024-06-04 15:29:06 +0530

    save

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { emailIcon, passwordIcon, userIcon } from "../Icons/Icons"; // Import all the icons
+import { emailIcon, passwordIcon, userIcon } from "../Icons/Icons";
 
 const Login = () => {
   const [password, setPassword] = useState("");
